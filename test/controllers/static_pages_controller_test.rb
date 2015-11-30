805352b0ab512b30de6c723a75c3bad64eca6a7b
Tom <tom.hockett@gmail.com>
2015-11-30 16:55:11 -0600

    finish static_pages structure

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,24 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | brine + barrel"
   end
 
   test "should get about" do
     get :about
     assert_response :success
+    assert_select "title", "About | brine + barrel"
+
   end
 
   test "should get news" do
     get :news
     assert_response :success
+    assert_select "title", "News | brine + barrel"
+
   end
 
   test "should get store" do
     get :store
     assert_response :success
-  end
+    assert_select "title", "Store | brine + barrel"
 
+  end
 end
