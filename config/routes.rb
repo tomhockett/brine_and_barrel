65862256a450ca3d9a34da50c85c97441a2ea3cd
Tom Hockett <tom.hockett@gmail.com>
2016-03-28 21:50:02 -0500

    refactor gemfile

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
 
-  root                      'static_pages#home'
-
+  root             'static_pages#home'
   get  'about'  => 'static_pages#about'
-
   get 'contact' => 'static_pages#contact'
-
   get 'news'    => 'static_pages#news'
-
-  get 'store'    => 'static_pages#store'
+  get 'store'   => 'static_pages#store'
 
 end
